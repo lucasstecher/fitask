b83775d1cfbceb8be0b98aa186a7e22f0d46e714
Lucas Stecher <lucasvieira0070@gmail.com>
2025-12-29 13:21:04 -0300

    :tada: chore: base project files

---
+++ src/main/java/dev/stecher/FiTask/FiTaskApplication.java
@@ -0,0 +1,13 @@
+package dev.stecher.FiTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/stecher/FiTask/FiTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.stecher.FiTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
