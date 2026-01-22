417576029420319868fd0ca49d9eba1682836d26
Lucas Stecher <lucasvieira0070@gmail.com>
2026-01-21 22:58:43 -0300

    create tables (migrations), add flyway dependency and update application.yaml

---
+++ src/main/resources/db/migration/V1__3_create_projects_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE projects (
+    id UUID PRIMARY KEY ,
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    owner_id UUID NOT NULL,
+    active BOOLEAN NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL,
+);
---
+++ src/main/resources/db/migration/V1__4_create_audit_logs_table.sql
@@ -0,0 +1,11 @@
+CREATE TYPE action as ENUM ('CREATE', 'UPDATE', 'DELETE')
+
+CREATE TABLE audit_logs (
+    id UUID PRIMARY KEY,
+    action action NOT NULL,
+    entity_type VARCHAR(255) NOT NULL,
+    entity_id UUID NOT NULL,
+    user_id UUID NOT NULL,
+    changes TEXT,
+    timestamp TIMESTAMPTZ NOT NULL
+);
