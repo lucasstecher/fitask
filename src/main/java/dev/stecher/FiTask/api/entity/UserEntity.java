594fabf78d711a1fe14de34bf83ba81fd1f0cb06
Lucas Stecher <lucasvieira0070@gmail.com>
2026-02-01 10:14:22 -0300

    generate getter and setter user entity

--- src/main/java/dev/stecher/FiTask/api/entity/UserEntity.java
+++ src/main/java/dev/stecher/FiTask/api/entity/UserEntity.java
@@ -62,4 +62,68 @@ public class UserEntity {
         PROJECT_MANAGER,
         DEVELOPER
     }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
 }
