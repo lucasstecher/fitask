ede958ae28ad3bed79d9e4f1818d20630918de9a
Lucas Stecher <lucasvieira0070@gmail.com>
2026-02-08 14:15:03 -0300

    update owner reference

--- src/main/resources/db/migration/V1_1_1__create_projects_table.sql
+++ src/main/resources/db/migration/V1_1_1__create_projects_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE projects (
     id UUID PRIMARY KEY ,
     name VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
-    owner_id UUID NOT NULL,
+    owner_id UUID NOT NULL REFERENCES users(id),
     active BOOLEAN NOT NULL,
     created_at TIMESTAMPTZ NOT NULL
 );
